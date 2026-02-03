cee2a253578f3d2da891e727cab177d19e6ff5c2
Raphael Margueron <raphael.margueron@gmail.com>
2026-02-03 15:44:01 +0100

    update backup script to work with borg

--- backup_db.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/zsh
-backupFolder=/mnt/samba/backups/baikal.theraphael0000.ch
-monthlyArchive=${backupFolder}/$(date +"%Y%m")_baikal.theraphael0000.ch.zip
-dbFile=/opt/baikal.theraphael0000.ch/data/db/db.sqlite
-tmpFile=/tmp/$(date +"%Y%m%d%H%M%S")_db.sqlite
-cp $dbFile $tmpFile && zip -j -u $monthlyArchive $tmpFile && rm $tmpFile
